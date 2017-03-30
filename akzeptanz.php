74a13405ebee6721457e0d3dd49c9bf725fa7b78
Maximilian Klose <Maximilian Klose>
2017-03-30 19:03:21 +0200

    changes to title

--- akzeptanz.php
+++ akzeptanz.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titel</title>
+    <title>Changes</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="scripts\bootstrap-3.3.7\dist\css\bootstrap.min.css">
 
