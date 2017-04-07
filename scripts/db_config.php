4107a43cc8f39430d956f1669e61f78f006c0a6e
bauernknecht <cptmcaoxlia@live.de>
2017-04-07 12:25:14 +0200

    scripts für datenbank

---
+++ scripts/db_config.php
@@ -0,0 +1,38 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create database
+$sql = "CREATE DATABASE userdaten";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "<br> Error creating database: " . mysqli_error($conn);
+}
+
+#Tabelle erstellen
+
+include("tabelleerstellen.php");
+
+#Datensätze init
+	$query_datecreate ="SELECT * FROM user WHERE user = '92106'";
+	$result = mysqli_query($conn, $query_datecreate);
+	$row = mysqli_fetch_array($result);
+	if(!$row){
+		include("datensaetze.php");
+	}
+	else{
+		echo " <br> Daten existieren schon!";
+	}
+	
+
+mysqli_close($conn);			
+?>
---
+++ scripts/tabelleerstellen.php
@@ -0,0 +1,38 @@
+<?php
+
+$dbname = "userdaten";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$sqluser ="CREATE TABLE user(
+    				`id` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    				`user` VARCHAR(40) NOT NULL,
+    				`password` VARCHAR(40) NOT NULL,
+					`frag1` VARCHAR(10) NOT NULL,
+					`frag2` VARCHAR(10) NOT NULL,
+					`frag3` VARCHAR(10) NOT NULL,
+					`frag4` VARCHAR(10) NOT NULL,
+					`frag5` VARCHAR(10) NOT NULL,
+					`frag6` VARCHAR(10) NOT NULL,
+					`frag7` VARCHAR(10) NOT NULL,
+					`frag8` VARCHAR(10) NOT NULL,
+					`frag9` VARCHAR(10) NOT NULL,
+					`frag10` VARCHAR(10) NOT NULL,
+					`frag11` VARCHAR(10) NOT NULL,
+					`frag12` VARCHAR(10) NOT NULL					
+    			) ENGINE = MYISAM;
+				";
+
+	if (mysqli_query($conn, $sqluser)) {
+	    echo "<br> Table user created successfully";
+	} else {
+	    echo "<br> Error creating table: " . mysqli_error($conn);
+	}
+
+?>
