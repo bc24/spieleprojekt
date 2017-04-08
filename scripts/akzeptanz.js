85a63d1a33985594db691ed56b67e232f260f383
Tischnee <wagnerfrosch@gmail.com>
2017-04-08 23:03:54 +0200

    akzeptanz
    allgemeines Desing fertig. Funtktion der Auswertung fehlt komplett
    (php). Es kann kein Bootstrap genutzt werden.

--- scripts/akzeptanz.js
+++ scripts/akzeptanz.js
@@ -1,1 +1,95 @@
-$('#Frage1Bomberman').html( $('#slider1Frage').value);
+//Frage 1
+$("#frage1Bomberman").change(function(){
+  $("#wertFrage1Bomberman").html( $('#frage1Bomberman').val())
+});
+$("#frage1Teeworld").change(function(){
+  $("#wertFrage1Teeworld").html( $('#frage1Teeworld').val())
+});
+
+//Frage 2
+$("#frage2Bomberman").change(function(){
+  $("#wertFrage2Bomberman").html( $('#frage2Bomberman').val())
+});
+$("#frage2Teeworld").change(function(){
+  $("#wertFrage2Teeworld").html( $('#frage2Teeworld').val())
+});
+
+//Frage 3
+$("#frage3Bomberman").change(function(){
+  $("#wertFrage3Bomberman").html( $('#frage3Bomberman').val())
+});
+$("#frage3Teeworld").change(function(){
+  $("#wertFrage3Teeworld").html( $('#frage3Teeworld').val())
+});
+
+//Frage 4
+$("#frage4Bomberman").change(function(){
+  $("#wertFrage4Bomberman").html( $('#frage4Bomberman').val())
+});
+$("#frage4Teeworld").change(function(){
+  $("#wertFrage4Teeworld").html( $('#frage4Teeworld').val())
+});
+
+//Frage 5
+$("#frage5Bomberman").change(function(){
+  $("#wertFrage5Bomberman").html( $('#frage5Bomberman').val())
+});
+$("#frage5Teeworld").change(function(){
+  $("#wertFrage5Teeworld").html( $('#frage5Teeworld').val())
+});
+
+//Frage 6
+$("#frage6Bomberman").change(function(){
+  $("#wertFrage6Bomberman").html( $('#frage6Bomberman').val())
+});
+$("#frage6Teeworld").change(function(){
+  $("#wertFrage6Teeworld").html( $('#frage6Teeworld').val())
+});
+
+//Frage 7
+$("#frage7Bomberman").change(function(){
+  $("#wertFrage7Bomberman").html( $('#frage7Bomberman').val())
+});
+$("#frage7Teeworld").change(function(){
+  $("#wertFrage7Teeworld").html( $('#frage7Teeworld').val())
+});
+
+//Frage 8
+$("#frage8Bomberman").change(function(){
+  $("#wertFrage8Bomberman").html( $('#frage8Bomberman').val())
+});
+$("#frage8Teeworld").change(function(){
+  $("#wertFrage8Teeworld").html( $('#frage8Teeworld').val())
+});
+
+//Frage 9
+$("#frage9Bomberman").change(function(){
+  $("#wertFrage9Bomberman").html( $('#frage9Bomberman').val())
+});
+$("#frage9Teeworld").change(function(){
+  $("#wertFrage9Teeworld").html( $('#frage9Teeworld').val())
+});
+
+//Frage 10
+$("#frage10Bomberman").change(function(){
+  $("#wertFrage10Bomberman").html( $('#frage10Bomberman').val())
+});
+$("#frage10Teeworld").change(function(){
+  $("#wertFrage10Teeworld").html( $('#frage10Teeworld').val())
+});
+
+//Frage 11
+$("#frage11Bomberman").change(function(){
+  $("#wertFrage11Bomberman").html( $('#frage11Bomberman').val())
+});
+$("#frage11Teeworld").change(function(){
+  $("#wertFrage11Teeworld").html( $('#frage11Teeworld').val())
+});
+
+//Frage 12
+$("#frage12Bomberman").change(function(){
+  $("#wertFrage12Bomberman").html( $('#frage12Bomberman').val())
+});
+$("#frage12Teeworld").change(function(){
+  $("#wertFrage12Teeworld").html( $('#frage12Teeworld').val())
+});
