835a32041b75085b8a030d5aae57edfc0ed7fe85
Tischnee <wagnerfrosch@gmail.com>
2017-04-10 19:21:56 +0200

    Akzept Anfang des php scripts für Datensätze init

--- akzeptanz.php
+++ akzeptanz.php
@@ -4,10 +4,9 @@ session_start();
 include 'akzeptanz/akzeptanz.html';
 include("scripts/connection.php");
 
-// if (isset($_POST['Absenden'])){
-//   $query ="SELECT user,password FROM user WHERE user = '".$_SESSION['username']."' AND password = '".$_SESSION['password']."'";
-//   $result = mysqli_query($link, $query);
-//
-// }
+if (isset($_POST['ergebnis'])){
+  $sql = "Update user Set frag1 = "'.$_Post['sliderFrage1Bomberman'].'" WHERE USER='1234'";
 
+}
+// Update user Set frag1 = "5" WHERE user="1234"
 ?>
