d72c1276276c5c64d22039c13575dd20ff54d569
Tischnee <wagnerfrosch@gmail.com>
2017-04-10 20:24:42 +0200

    ÄNDERUNG IN DER DB_CONFIG. DB neu generieren!!!!!
    Es wurden neue Spalten eingefüht um Teeworld und Bomberman zu
    unterscheiden

--- scripts/tabelleerstellen.php
+++ scripts/tabelleerstellen.php
@@ -14,21 +14,32 @@ $sqluser ="CREATE TABLE user(
     				`id` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     				`user` VARCHAR(40) NOT NULL,
     				`password` VARCHAR(40) NOT NULL,
-					`frag1` VARCHAR(10) NOT NULL,
-					`frag2` VARCHAR(10) NOT NULL,
-					`frag3` VARCHAR(10) NOT NULL,
-					`frag4` VARCHAR(10) NOT NULL,
-					`frag5` VARCHAR(10) NOT NULL,
-					`frag6` VARCHAR(10) NOT NULL,
-					`frag7` VARCHAR(10) NOT NULL,
-					`frag8` VARCHAR(10) NOT NULL,
-					`frag9` VARCHAR(10) NOT NULL,
-					`frag10` VARCHAR(10) NOT NULL,
-					`frag11` VARCHAR(10) NOT NULL,
-					`frag12` VARCHAR(10) NOT NULL					
+					`frag1b` VARCHAR(10) NOT NULL,
+          `frag1t` VARCHAR(10) NOT NULL,
+					`frag2b` VARCHAR(10) NOT NULL,
+          `frag2t` VARCHAR(10) NOT NULL,
+					`frag3b` VARCHAR(10) NOT NULL,
+          `frag3t` VARCHAR(10) NOT NULL,
+					`frag4b` VARCHAR(10) NOT NULL,
+          `frag4t` VARCHAR(10) NOT NULL,
+					`frag5b` VARCHAR(10) NOT NULL,
+          `frag5t` VARCHAR(10) NOT NULL,
+					`frag6b` VARCHAR(10) NOT NULL,
+          `frag6t` VARCHAR(10) NOT NULL,
+					`frag7b` VARCHAR(10) NOT NULL,
+          `frag7t` VARCHAR(10) NOT NULL,
+					`frag8b` VARCHAR(10) NOT NULL,
+          `frag8t` VARCHAR(10) NOT NULL,
+					`frag9b` VARCHAR(10) NOT NULL,
+          `frag9t` VARCHAR(10) NOT NULL,
+					`frag10b` VARCHAR(10) NOT NULL,
+          `frag10t` VARCHAR(10) NOT NULL,
+					`frag11b` VARCHAR(10) NOT NULL,
+          `frag11t` VARCHAR(10) NOT NULL,
+					`frag12b` VARCHAR(10) NOT NULL,
+          `frag12t` VARCHAR(10) NOT NULL
     			) ENGINE = MYISAM;
 				";
-
 	if (mysqli_query($conn, $sqluser)) {
 	    echo "<br> Table user created successfully";
 	} else {
