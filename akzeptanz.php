7d3df9a297d2c45b85f531d0f340cf4cda9d3691
Tischnee <wagnerfrosch@gmail.com>
2017-04-10 20:58:53 +0200

    Datensätze einfügen in die DB
    Der Datensatz für die Frage 11 Bomberman geht nicht. Teeworld fehlt noch
    komplett

--- akzeptanz.php
+++ akzeptanz.php
@@ -5,8 +5,15 @@ include 'akzeptanz/akzeptanz.html';
 include("scripts/connection.php");
 
 if (isset($_POST['ergebnis'])){
-  $sql = "Update user Set frag1 = "'.$_Post['sliderFrage1Bomberman'].'" WHERE USER='1234'";
-
+  // Bomberman Frage 1 - 6
+  $sql = "UPDATE user SET frag1b = '".$_POST['sliderFrage1Bomberman']."' , frag2b = '".$_POST['sliderFrage2Bomberman']."' , frag3b = '".$_POST['sliderFrage3Bomberman']."' , frag4b = '".$_POST['sliderFrage4Bomberman']."' , frag5b = '".$_POST['sliderFrage5Bomberman']."' , frag6b = '".$_POST['sliderFrage6Bomberman']."' WHERE user='".$_SESSION['username']."'";
+  // Bomberman Grage 7 -12
+  $sql2 = "UPDATE user SET frag7b = '".$_POST['sliderFrage7Bomberman']."' , frag8b = '".$_POST['sliderFrage8Bomberman']."' , frag9b = '".$_POST['sliderFrage9Bomberman']."' , frag10b = '".$_POST['sliderFrage10Bomberman']."' , frag12b = '".$_POST['verbesserungText']."' WHERE user='".$_SESSION['username']."'";
+  $sql3 = "UPDATE user SET frag11b = '".$_POST['sliderFrage11Bomberman']."' WHERE user='".$_SESSION['username']."'";
+  // Frage 11 Bomerbman wird nicht gesendet ????? Der rest ??
+  $result = mysqli_query($link, $sql);
+  $result = mysqli_query($link, $sql2);
+  $result = mysqli_query($link, $sql3);
 }
 // Update user Set frag1 = "5" WHERE user="1234"
 ?>
