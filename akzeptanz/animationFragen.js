db13faff50cc7f9d1bf0da0a4463da7dfe20244a
Tischnee <wagnerfrosch@gmail.com>
2017-04-10 16:01:37 +0200

    akzep

--- akzeptanz/animationFragen.js
+++ akzeptanz/animationFragen.js
@@ -14,27 +14,40 @@ var frage12offen = true;
 
 /*Animation der Fragen*/
 $("#pfeil1").click(function(){
-  $("#sliderbox1").animate({height: "toggle"}, 1000, function() {});
-  $("#sliderbox2").animate({height: "toggle"}, 1000, function() {});
+  $("#sliderbox1").animate({opacity: "toggle"}, 1000, function() {});
+  $("#sliderbox2").animate({opacity: "toggle"}, 1000, function() {});
   if (frage1offen){
-    $("#blockanfang").animate({height:"130px"}, 1000, function() {});
+    $("#blockanfang").animate({height:"90px"}, 1000, function() {});
     frage1offen = false;
   }
   else{
-    $("#blockanfang").animate({height:"300px"}, 1000, function() {});
+    $("#blockanfang").animate({height:"260px"}, 1000, function() {});
     frage1offen = true;
   }
 });
 
 $("#pfeil2").click(function(){
-  $("#sliderbox3").animate({height: "toggle"}, 1000, function() {});
-  $("#sliderbox4").animate({height: "toggle"}, 1000, function() {});
+  $("#sliderbox3").animate({opacity: "toggle"}, 1000, function() {});
+  $("#sliderbox4").animate({opacity: "toggle"}, 1000, function() {});
   if (frage2offen){
-    $("#block2").animate({height:"130px"}, 1000, function() {});
+    $("#block2").animate({height:"90px"}, 1000, function() {});
     frage2offen = false;
   }
   else{
-    $("#block2").animate({height:"300px"}, 1000, function() {});
+    $("#block2").animate({height:"260px"}, 1000, function() {});
     frage2offen = true;
+  }
+});
+
+$("#pfeil3").click(function(){
+  $("#sliderbox5").animate({opacity: "toggle"}, 1000, function() {});
+  $("#sliderbox6").animate({opacity: "toggle"}, 1000, function() {});
+  if (frage2offen){
+    $("#block3").animate({height:"90px"}, 1000, function() {});
+    frage3offen = false;
+  }
+  else{
+    $("#block3").animate({height:"260px"}, 1000, function() {});
+    frage3offen = true;
   }
 });
