9e4eaabb5930d635915ff08e77b08240de276a5c
Tischnee <wagnerfrosch@gmail.com>
2017-04-12 23:34:07 +0200

    Akzeptanz Veränderung. Slider funktion erweitert

--- akzeptanz/scripts/akzeptanz.js
+++ akzeptanz/scripts/akzeptanz.js
@@ -1,95 +1,95 @@
 //Frage 1
-$("#frage1Bomberman").change(function(){
+$("#frage1Bomberman").on("change mousemove", function(){
   $("#wertFrage1Bomberman").html( $('#frage1Bomberman').val())
 });
-$("#frage1Teeworld").change(function(){
+$("#frage1Teeworld").on("change mousemove",function(){
   $("#wertFrage1Teeworld").html( $('#frage1Teeworld').val())
 });
 
 //Frage 2
-$("#frage2Bomberman").change(function(){
+$("#frage2Bomberman").on("change mousemove",function(){
   $("#wertFrage2Bomberman").html( $('#frage2Bomberman').val())
 });
-$("#frage2Teeworld").change(function(){
+$("#frage2Teeworld").on("change mousemove",function(){
   $("#wertFrage2Teeworld").html( $('#frage2Teeworld').val())
 });
 
 //Frage 3
-$("#frage3Bomberman").change(function(){
+$("#frage3Bomberman").on("change mousemove",function(){
   $("#wertFrage3Bomberman").html( $('#frage3Bomberman').val())
 });
-$("#frage3Teeworld").change(function(){
+$("#frage3Teeworld").on("change mousemove",function(){
   $("#wertFrage3Teeworld").html( $('#frage3Teeworld').val())
 });
 
 //Frage 4
-$("#frage4Bomberman").change(function(){
+$("#frage4Bomberman").on("change mousemove",function(){
   $("#wertFrage4Bomberman").html( $('#frage4Bomberman').val())
 });
-$("#frage4Teeworld").change(function(){
+$("#frage4Teeworld").on("change mousemove",function(){
   $("#wertFrage4Teeworld").html( $('#frage4Teeworld').val())
 });
 
 //Frage 5
-$("#frage5Bomberman").change(function(){
+$("#frage5Bomberman").on("change mousemove",function(){
   $("#wertFrage5Bomberman").html( $('#frage5Bomberman').val())
 });
-$("#frage5Teeworld").change(function(){
+$("#frage5Teeworld").on("change mousemove",function(){
   $("#wertFrage5Teeworld").html( $('#frage5Teeworld').val())
 });
 
 //Frage 6
-$("#frage6Bomberman").change(function(){
+$("#frage6Bomberman").on("change mousemove",function(){
   $("#wertFrage6Bomberman").html( $('#frage6Bomberman').val())
 });
-$("#frage6Teeworld").change(function(){
+$("#frage6Teeworld").on("change mousemove",function(){
   $("#wertFrage6Teeworld").html( $('#frage6Teeworld').val())
 });
 
 //Frage 7
-$("#frage7Bomberman").change(function(){
+$("#frage7Bomberman").on("change mousemove",function(){
   $("#wertFrage7Bomberman").html( $('#frage7Bomberman').val())
 });
-$("#frage7Teeworld").change(function(){
+$("#frage7Teeworld").on("change mousemove",function(){
   $("#wertFrage7Teeworld").html( $('#frage7Teeworld').val())
 });
 
 //Frage 8
-$("#frage8Bomberman").change(function(){
+$("#frage8Bomberman").on("change mousemove",function(){
   $("#wertFrage8Bomberman").html( $('#frage8Bomberman').val())
 });
-$("#frage8Teeworld").change(function(){
+$("#frage8Teeworld").on("change mousemove",function(){
   $("#wertFrage8Teeworld").html( $('#frage8Teeworld').val())
 });
 
 //Frage 9
-$("#frage9Bomberman").change(function(){
+$("#frage9Bomberman").on("change mousemove",function(){
   $("#wertFrage9Bomberman").html( $('#frage9Bomberman').val())
 });
-$("#frage9Teeworld").change(function(){
+$("#frage9Teeworld").on("change mousemove",function(){
   $("#wertFrage9Teeworld").html( $('#frage9Teeworld').val())
 });
 
 //Frage 10
-$("#frage10Bomberman").change(function(){
+$("#frage10Bomberman").on("change mousemove",function(){
   $("#wertFrage10Bomberman").html( $('#frage10Bomberman').val())
 });
-$("#frage10Teeworld").change(function(){
+$("#frage10Teeworld").on("change mousemove",function(){
   $("#wertFrage10Teeworld").html( $('#frage10Teeworld').val())
 });
 
 //Frage 11
-$("#frage11Bomberman").change(function(){
+$("#frage11Bomberman").on("change mousemove",function(){
   $("#wertFrage11Bomberman").html( $('#frage11Bomberman').val())
 });
-$("#frage11Teeworld").change(function(){
+$("#frage11Teeworld").on("change mousemove",function(){
   $("#wertFrage11Teeworld").html( $('#frage11Teeworld').val())
 });
 
 //Frage 12
-$("#frage12Bomberman").change(function(){
+$("#frage12Bomberman").on("change mousemove",function(){
   $("#wertFrage12Bomberman").html( $('#frage12Bomberman').val())
 });
-$("#frage12Teeworld").change(function(){
+$("#frage12Teeworld").on("change mousemove",function(){
   $("#wertFrage12Teeworld").html( $('#frage12Teeworld').val())
 });
